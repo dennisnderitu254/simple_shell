0a2b2350fc2122ebaecadfb307e51d61bb739352
dennisnderitu254 <dknderitu@gmail.com>
2022-11-06 22:22:11 +0300

    adding parser description

--- parser.c
+++ parser.c
@@ -1,6 +1,11 @@
 #include "shell.h"
 
 /**
+ * Parsing, syntax analysis, or syntactic analysis is the process of analyzing a string of symbols, 
+ * either in natural language, computer languages or data structures
+*/
+
+/**
  * is_cmd - determines if a file is an executable command
  * @info: the info struct
  * @path: path to the file
