88d1c034c282d5bd37c3aa2cc70219789da28ec7
dennisnderitu254 <dknderitu@gmail.com>
2022-11-09 13:23:31 +0300

    adding description to fork

--- shell_loop.c
+++ shell_loop.c
@@ -125,6 +125,15 @@ void find_cmd(info_t *info)
  *
  * Return: void
  */
+
+/**
+ * Fork system call is used for creating a new process, which is called child process,
+ *  which runs concurrently with the process that makes the fork() call (parent process).
+ *  After a new child process is created, 
+ * both processes will execute the next instruction following the fork() system call.
+ *  A child process uses the same pc(program counter), 
+ * same CPU registers, same open files which use in the parent process.
+*/
 void fork_cmd(info_t *info)
 {
 	pid_t child_pid;
